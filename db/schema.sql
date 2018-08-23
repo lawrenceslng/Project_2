c622808dc19e076ab761fdb349267e4cb7867e4a
LSLNG <lawrencesuilunng@gmail.com>
2018-08-22 20:48:49 -0700

    more comments

--- db/schema.sql
+++ db/schema.sql
@@ -5,12 +5,13 @@ CREATE DATABASE WebDevFlashcard_db;
 USE WebDevFlashcard_db;
 
 -- table for all users (login system table)
+-- I am going to follow the guidelines in the video I sent you guys for setting the data types of this table to make authentication and other things dealing with login system work
 CREATE TABLE users(
 	id INT NOT NULL AUTO_INCREMENT,
-	username VARCHAR(30) NOT NULL,
+	username VARCHAR(30) NOT NULL,    
 	password BINARY NOT NULL,
 	email VARCHAR(100) NOT NULL,
-	accountType VARCHAR(30) NOT NULL,
+	accountType VARCHAR(30) NOT NULL,	-- this specifies whether the user is a student, instructor, or administrator
 	PRIMARY KEY (id)
 );
 
