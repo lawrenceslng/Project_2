497cd05038a3faa62fdbbd69191da1cc5459a9dd
LSLNG <lawrencesuilunng@gmail.com>
2018-08-25 09:55:37 -0700

    added notes and some additional pages

---
+++ layout_notes.md
@@ -0,0 +1,23 @@
+Web Layout
+
+- Home Page
+    - Header of "Home" "Register" "Login"
+    - Jumbotron slideshow showcasing features
+    - Subheader content boxes of features 
+    - Footer
+- Register Page
+    - username
+    - email
+    - password
+    - re-enter password
+    - account type
+- Login Page
+    - username
+    - password
+
+Restricted Pages
+    - Profile
+    - Interactive Flashcards
+    - Lesson 
+    - Code Snippets
+    - User Interactive Area
--- server.js
+++ server.js
@@ -24,21 +24,24 @@ app.set('view engine', 'ejs');
 
 // Initializes the connection variable to sync with a MySQL database
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
   
     // Your port; if not 3306
     port: 3306,
   
     // Your username
-    user: "root",
+    user: process.env.DB_USER,
   
     // Your password
-    password: process.env.Password,  //placeholder for your own mySQL password that you store in your own .env file
-    database: "WebDevFlashcard_db"    //TBD
+    password: process.env.DB_PASSWORD,  //placeholder for your own mySQL password that you store in your own .env file
+    database: process.env.DB_NAME    //TBD
   });
 
 
-
+//routes  
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
 
 
 
