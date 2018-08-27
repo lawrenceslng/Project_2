b7e2dd6fcc8115e4cb9cd66d231bef0968f540f3
tian3401 <matt.chen3401@yahoo.com>
2018-08-27 15:21:00 -0700

    created usersname and flashcards

---
+++ db/seed.sql
@@ -0,0 +1,26 @@
+INSERT INTO users(username, first_name,last_name,email,password, account_type)
+VALUES("mchen88", "Matthew", "Chen", "matt.chen3401@yahoo.com", "tian3401", "Administrator");
+
+INSERT INTO cards(category,front,back,difficulty)
+VALUES("HTML", "What is the syntax for sourcing an image file?", "<img src="" alt"">", "Easy"); 
+
+INSERT INTO cards(category,front,back,difficulty)
+VALUES("HTML", "What is the difference between a span and div element?", "span elements are inline element whereas a div is a block element.", "Medium");
+
+INSERT INTO cards(category,front,back,difficulty)
+VALUES("jQuery", "What is jQuery?", "jQuery is a Javascript library that simplifies code through the use of jQuery methods", "Easy");
+
+INSERT INTO cards(category,front,back,difficulty)
+VALUES("jQuery", "What is the syntax for a click event?","$('#target').on('click', function(){});", "Medium");
+
+INSERT INTO cards(category,front,back,difficulty)
+VALUES("AJAX", "What is AJAX", "AJAX stands for Asynchronus Javascript and XML. It is a client-side script that exchanges data with servers and updates webpages without the need to reload the entire page.", "Medium");
+
+INSERT INTO cards(category,front,back,difficulty)
+VALUES("AJAX", "What is an Asynchronous AJAX request", "An Asynchronous AJAX request is a request that does not wait for a server to reply and handles the response as it comes, allowing other components of the page to continue to process", "Hard");
+
+INSERT INTO cards(category,front,back,difficulty)
+VALUES("mySQL", "What are joins?", "Joins are commands that are used to query data from two or more tables", "Easy");
+
+INSERT INTO cards(category,front,back,difficulty)
+VALUES("mySQL", "What do DDL, DML, and DCL stand for?", "DDL stands for Data Definition Language e.g. CREATE TABLE command, DML stands for Data Manipulation Language e.g. SELECT, INSERT commands, DCL stands for Data Control Language e.g. GRANT, EVOKE commands>", "Hard");
