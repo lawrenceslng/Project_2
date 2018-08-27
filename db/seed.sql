2e09750d4862787107352f8e75259deb8ad1c86c
LSLNG <lawrencesuilunng@gmail.com>
2018-08-26 22:09:04 -0700

    seeds for CSS etc

---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE users(
+-- 	id INT NOT NULL AUTO_INCREMENT,
+-- 	username VARCHAR(30) NOT NULL,
+-- 	first_name VARCHAR(255) NOT NULL,
+-- 	last_name VARCHAR (255) NOT NULL, 
+-- 	email VARCHAR(100) NOT NULL,   
+-- 	password BINARY NOT NULL,
+-- 	account_type ENUM('Student', 'Instructor', 'Administrator') NOT NULL,	-- this specifies whether the user is a student, instructor, or administrator
+-- 	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+-- 	PRIMARY KEY (id)
+-- );
+
+INSERT INTO users (username, first_name, last_name, email, password, account_type)
+VALUES()
