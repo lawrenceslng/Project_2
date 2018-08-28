fb52b45393c1e60a8e392404550d69b3415de06e
ava krishn <avakrishn@gmail.com>
2018-08-27 19:29:00 -0700

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,30 @@
 DROP DATABASE IF EXISTS WebDevFlashcard_db;
 CREATE DATABASE WebDevFlashcard_db;
 USE WebDevFlashcard_db;
+
 -- table for all users (login system table)
 -- I am going to follow the guidelines in the video I sent you guys for setting the data types of this table to make authentication and other things dealing with login system work
+
 CREATE TABLE users(
-    id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(30) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR (255) NOT NULL, 
-    email VARCHAR(100) NOT NULL, 
-    password BINARY NOT NULL,
-    account_type ENUM('Student', 'Instructor', 'Administrator') NOT NULL,   -- this specifies whether the user is a student, instructor, or administrator
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+	id INT NOT NULL AUTO_INCREMENT,
+	username VARCHAR(30) NOT NULL,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR (255) NOT NULL, 
+	email VARCHAR(100) NOT NULL, 
+	password BINARY NOT NULL,
+	account_type ENUM('Student', 'Instructor', 'Administrator') NOT NULL,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
 );
+
 -- each user has a profile that they can store information (like a short bio, contact info); we can create another table and link it to the users table
 CREATE TABLE userProfile(
-    id INT NOT NULL AUTO_INCREMENT,
-    users_id INT NOT NULL,
-    PRIMARY KEY (id),
-FOREIGN KEY (users_id) REFERENCES users(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	users_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (users_id) REFERENCES users(id)
 );
+
 -- table that holds all the cards (the category and subcategory that the card belongs to, front: question, back: answer, and how many votes each card gets from the community and the creator_id is the users_id of the person who created the card)
 CREATE TABLE cards(
 	id INT NOT NULL AUTO_INCREMENT,
@@ -32,8 +36,9 @@ CREATE TABLE cards(
 	creator_id INT NOT NULL,
 	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id),
-  FOREIGN KEY (creator_id) REFERENCES users(id)
+	FOREIGN KEY (creator_id) REFERENCES users(id)
 );
+
 -- CREATE TABLE subcategories(
 --  id INT NOT NULL AUTO_INCREMENT,
 --  cards_id INT NOT NULL,
@@ -41,30 +46,33 @@ CREATE TABLE cards(
 --  PRIMARY KEY (id),
 --  FOREIGN KEY (cards_id) REFERENCES cards(id)
 -- );
+
 -- table holds all the decks (the users_id is the foreign key that links the deck to the user)
 CREATE TABLE decks(
-    id INT NOT NULL AUTO_INCREMENT,
-    users_id INT NOT NULL,
-    name VARCHAR (255) NOT NULL,
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-FOREIGN KEY (users_id) REFERENCES users(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	users_id INT NOT NULL,
+	name VARCHAR (255) NOT NULL,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+	FOREIGN KEY (users_id) REFERENCES users(id)
 );
+
 -- table inks the decks and the cards such that each deck can have multiple cards and each card can be used in multiple decks
 CREATE TABLE deck_cards(
-    decks_id INT NOT NULL,
-    cards_id INT NOT NULL,
-    FOREIGN KEY (decks_id) REFERENCES decks(id),
-    FOREIGN KEY (cards_id) REFERENCES cards(id)
+	decks_id INT NOT NULL,
+	cards_id INT NOT NULL,
+	FOREIGN KEY (decks_id) REFERENCES decks(id),
+	FOREIGN KEY (cards_id) REFERENCES cards(id)
 );
+
 -- table that holds all the comments and references the user who created the comment and the card the comment if for
 CREATE TABLE comments(
-    id INT NOT NULL AUTO_INCREMENT,
-    users_id INT NOT NULL,
-    cards_id INT NOT NULL,
-    comment TEXT NOT NULL,
-    date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (users_id) REFERENCES users(id),
-    FOREIGN KEY (cards_id) REFERENCES cards(id)
+	id INT NOT NULL AUTO_INCREMENT,
+	users_id INT NOT NULL,
+	cards_id INT NOT NULL,
+	comment TEXT NOT NULL,
+	date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id),
+	FOREIGN KEY (users_id) REFERENCES users(id),
+	FOREIGN KEY (cards_id) REFERENCES cards(id)
 );
