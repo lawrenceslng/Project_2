4dd08324573ce8394fedd59e2866a56ad1d7461e
ava krishn <avakrishn@gmail.com>
2018-08-30 21:19:33 -0700

    update allows user to edit the text of their created flashcards and saves to database

--- public/assets/javascript/cards.js
+++ public/assets/javascript/cards.js
@@ -1,23 +1,25 @@
 //----------------------------------Global Variables
 var i = 0;
-var length, URL;
+var length, URL, currentText;
+var canSwitch = true;
 
 //----------------------------------Functions
 
 // functions empties the div that contains the flashcard before invoking AJAX query to repopulate card with properties from the object at index int
 function updateCard(int){
-    $('.flashcard').empty();
+    $('.front, .back').empty();
     $.ajax({
         url: URL,
         method: 'GET'
     }).then(function(res){
         length = res.length;
         console.log(res);
-        var category = $("<p>").html("Category: " + res[int].category);
-        var front = $("<p>").html("Front: " + res[int].front);
-        var back = $("<p>").html("Back: " + res[int].back);
-
-        $('.flashcard').append(category, front, back);
+        var category = $("<p>").html(res[int].category).addClass('edit').attr('data-name', 'category').attr('data-id', res[int].id);
+        var front = $("<p>").html(res[int].front).addClass('edit').attr('data-name', 'front').attr('data-id', res[int].id);
+        var back = $("<p>").html(res[int].back).addClass('edit').attr('data-name', 'back').attr('data-id', res[int].id);
+        var difficulty = $("<p>").html(res[int].difficulty).addClass('edit').attr('data-name', 'difficulty').attr('data-id', res[int].id);
+        $('.front').append(category, front);
+        $('.back').append(back, difficulty);
     });
 }
  
@@ -37,12 +39,56 @@ function add(){
     }
 }
 
+//function allows user to edit the text on page (when they double click on text)
+function editText(){
+    currentText = $(this).text();
+    // contenteditable allows you to have the user edit the content of the page
+    $(this).attr('contenteditable','true');
+    canSwitch = false;
+    $(this).on('keypress',doneEditText);
+}
+
+//when key is pressed if key = return or enter then html is no longer editable by user
+// if user presses key enter and there are only white spaces typed then the text reverts back to previous text
+function doneEditText(event){
+    var y = event.which || event.keyCode;
+    if(y == 13){
+        $(this).attr('contenteditable','false');
+        canSwitch = true;
+        if($(this).text().trim().length < 1){
+            $(this).text(currentText);
+        }
+        else{
+            
+            colkey = $(this).attr('data-name');
+            colval = $(this).text();
+            idkey = "id"
+            idval = $(this).attr('data-id');
+
+            var dataOb = {};
+            dataOb[colkey]= colval;
+            dataOb["id"] = idval;
+
+            $.ajax({
+                url: '/flashcards/edit',
+                method: 'PUT',
+                data: dataOb
+            }).then(function(res){
+                console.log(res);
+            });
+        }
+    }
+}
+
 // when the page loads and the window is ready, the updateCard function is executed with variable i= 0 as its argument
 $(window).ready(function(){
     // window.location.href returns the href (URL) of the current page
     // setting the URL request for ajax query based on the href of the current page
     if (window.location.href.split('/flashcards/')[1] == 'all_cards'){
-        URL = '/flashcards/community_cards/';
+        URL = '/flashcards/community_cards';
+    }else if(window.location.href.split('/flashcards/')[1] == "" ){
+        URL = '/flashcards/view_cards'
+        $(document).on('click', '.edit', editText);
     }
     console.log(i);
     updateCard(i);
@@ -50,15 +96,21 @@ $(window).ready(function(){
 });
 
 // if the left key on keyboard is pressed then the subtract function is run and if the right key on the keyboard is pressed then the add function is run
-$("body").keydown(function(e){
+$(document).on('keydown', function(){
+    if(canSwitch){
+        switchCards();
+    }
+});
+    
+function switchCards(){
     // left arrow
-    if ((e.keyCode || e.which) == 37)
+    if ((event.keyCode || event.which) == 37)
     {   
         subtract();
     }
     // right arrow
-    if ((e.keyCode || e.which) == 39)
+    if ((event.keyCode || event.which) == 39)
     {
         add();
     }   
-});
+}
